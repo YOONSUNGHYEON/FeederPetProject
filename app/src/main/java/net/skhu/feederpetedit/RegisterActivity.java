d915a0a2d7a10cc6304ac5ce87ac884c438c9518
본인 GitHub 아이디[출처] <qead753951@naver.com>
2018-11-14 00:30:07 +0900

    modifiedRegisterActivity

--- app/src/main/java/net/skhu/feederpetedit/RegisterActivity.java
+++ app/src/main/java/net/skhu/feederpetedit/RegisterActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -32,44 +34,60 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String userID = idText.getText().toString();
-                final String userPassword = passwordText.getText().toString();
-                final String userName = nameText.getText().toString();
-                final int userAge = Integer.parseInt(ageText.getText().toString());
-                final int petType = 0;
-                final String petName = "";
 
-                final Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if (success) {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("회원등록에 성공했습니다.")
-                                        .setPositiveButton("확인", null)
-                                        .create()
-                                        .show();
-                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                RegisterActivity.this.startActivity(intent);
+
+                if(TextUtils.isEmpty(idText.getText()) || TextUtils.isEmpty(passwordText.getText())
+                        || TextUtils.isEmpty(nameText.getText()) || TextUtils.isEmpty(ageText.getText())){
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(RegisterActivity.this);
+                    builder1.setMessage("모든 정보를 입력해주세요.")
+                            .setNegativeButton("확인",null)
+                            .create()
+                            .show();
+                }
+                else
+                {
+                    final String userID = idText.getText().toString();
+                    final String userPassword = passwordText.getText().toString();
+                    final String userName = nameText.getText().toString();
+                    final int userAge = Integer.parseInt(ageText.getText().toString());
+                    final int petType = 0;
+                    final String petName = "";
+
+                    final Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                if (success) {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                    builder.setMessage("회원등록에 성공했습니다.")
+                                            .setPositiveButton("확인", null)
+                                            .create()
+                                            .show();
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    RegisterActivity.this.startActivity(intent);
+                                }
+                                else {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                    builder.setMessage("회원등록에 실패했습니다.")
+                                            .setNegativeButton("다시 시도", null)
+                                            .create()
+                                            .show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                            else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("회원등록에 실패했습니다.")
-                                        .setNegativeButton("다시 시도", null)
-                                        .create()
-                                        .show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
                         }
-                    }
 
-                };
-                RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userName, userAge, petType, petName, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
-                queue.add(registerRequest);
+                    };
+                    RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, userName, userAge, petType, petName, responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                    queue.add(registerRequest);
+
+                }
+
+
             }
         });
 
